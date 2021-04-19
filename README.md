7a29afc3f011e19185c1e02894ea52f4157f255e
G-Lazer <joshglazer@outlook.com>
2021-04-19 00:36:40 -0400

    Corrected syntax comment. Added the ability to scan by hostname. Added an exception for gaierror (hostname resolution failure).

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 The goal of friendlyScanner is to be a port scanner that walks the user through options for their scan.
 
 **Future Release Ideas**: 
-Scanning by hostname, not just IP.
 SYN and FIN scanning.
+Scanning a subnet by CIDR, and providing an output of open ports for each machine found.
--- friendlyScanner.py
+++ friendlyScanner.py
@@ -3,7 +3,7 @@
 import sys
 import socket
  
-# syntax = python3 friendlyScanner [ipv4]
+# syntax = python3 friendlyScanner.py [ipv4]
  
 try:
     portStart = int(input("\nEnter the starting port: "))
@@ -13,12 +13,18 @@ try:
 except ValueError:
     print("\nYou must input ports by number.")
  
-address = (sys.argv[1])
- 
-for port in range(portStart,portEnd):
-    x = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    response = x.connect_ex((address,port))
-    if response == 0:
-        print("Port {}: Open".format(port))
-    x.close()
+address = socket.gethostbyname(sys.argv[1])
+
+try:
+    
+    for port in range(portStart,portEnd):
+        x = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        response = x.connect_ex((address,port))
+        if response == 0:
+            print("Port {}: Open".format(port))
+        x.close()
+
+except socket.gaierror:
+    print("Couldn't resolve hostname.")
+
