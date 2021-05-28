617a5a5fe561b58de472dd05abcb38cfdf18da39
G-Lazer <joshglazer@outlook.com>
2021-05-28 01:46:08 -0400

    Updated to better follow PEP8 guidelines.

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 **Goal**: 
 The goal of friendlyScanner is to be a menu driven port scanner.
 
-**Future Release Ideas**: 
-SYN and FIN scanning.
+**Planned Updates**: 
+FIN scanning.
 Scanning a subnet by CIDR, and providing an output of open ports for each machine found.
--- friendlyScanner.py
+++ friendlyScanner.py
@@ -1,30 +1,29 @@
 #!/bin/python
- 
+
 import sys
 import socket
- 
+
 # syntax = python3 friendlyScanner.py [ipv4]
- 
+
 try:
-    portStart = int(input("\nEnter the starting port: "))
-    portEnd = int(input("Enter the ending port: "))
+    port_start = int(input("\nEnter the starting port: "))
+    port_end = int(input("Enter the ending port: "))
     print("\n")
- 
+
 except ValueError:
     print("\nYou must input ports by number.")
- 
+
 address = socket.gethostbyname(sys.argv[1])
 
 try:
-    
-    for port in range(portStart,portEnd):
+
+    for port in range(port_start, port_end):
         x = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
-        response = x.connect_ex((address,port))
+        response = x.connect_ex((address, port))
         if response == 0:
             print("Port {}: Open".format(port))
         x.close()
 
 except socket.gaierror:
-    print("Couldn't resolve hostname.")
-
+    print("Couldn't resolve hostname.\n")
